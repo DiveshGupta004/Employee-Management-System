01dac50e57f819f278d6d7150deae7aab10b890f
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-02-24 11:19:16 +0530

    updated the designationRoutes

--- backend/routes/designationRoutes.js
+++ backend/routes/designationRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const designationController = require('../controllers/designationController');
+const { authenticateAdmin } = require("../middleware/adminMiddleware");
 
-router.get('/', designationController.getAllDesignations);
-router.get('/:id', designationController.getDesignationById);
-router.post('/', designationController.createDesignation);
-router.put('/:id', designationController.updateDesignation);
-router.delete('/:id', designationController.deleteDesignation);
+router.get('/', authenticateAdmin ,designationController.getAllDesignations);
+router.get('/:id', authenticateAdmin ,designationController.getDesignationById);
+router.post('/', authenticateAdmin ,designationController.createDesignation);
+router.put('/:id', authenticateAdmin ,designationController.updateDesignation);
+router.delete('/:id', authenticateAdmin ,designationController.deleteDesignation);
 
 module.exports = router;
