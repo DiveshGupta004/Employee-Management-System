91214f76d1e1b32b16fae04c71ff750ec0303957
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-02-24 11:17:29 +0530

    updated the employeeController

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const employeeController = require('../controllers/employeeController');
+const express = require("express");
+const employeeController = require("../controllers/employeeController");
+const { authenticateAdmin } = require("../middleware/adminMiddleware");
 
-router.get('/', employeeController.getAllEmployees);
-router.post('/', employeeController.createEmployee);
-router.get('/:id', employeeController.getEmployeeById);
-router.put('/:id', employeeController.updateEmployee);
-router.delete('/:id', employeeController.deleteEmployee);
+const router = express.Router();
+
+
+router.post("/login", employeeController.login);
+
+// Admin-only routes
+router.post("/", authenticateAdmin, employeeController.createEmployee);
+router.get("/", authenticateAdmin, employeeController.getAllEmployees);
+router.get("/:id", authenticateAdmin, employeeController.getEmployeeById);
+router.put("/:id", authenticateAdmin, employeeController.updateEmployee);
+router.delete("/:id", authenticateAdmin, employeeController.deleteEmployee);
 
 module.exports = router;
